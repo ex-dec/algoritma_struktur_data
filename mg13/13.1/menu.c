d3ab5e617dde70173b685cd63e5b9a3fb379b4b3
krisna wahyu setyawan <50494089+ex-dec@users.noreply.github.com>
2022-05-17 00:44:37 -0700

    update menu

--- mg13/13.1/menu.c
+++ mg13/13.1/menu.c
@@ -22,12 +22,30 @@ int main(){
 
 char menu(int arr[]){
     char pilihan;
+    int cari;
     puts("Menu Search");
     puts("1. Sequential Search - unsorted");
     puts("2. Sequential Search - sorted");
     puts("3. Binary Search - sorted");
     printf("Masukkan pilihan anda : ");
-    scanf("");
+    scanf("%d", &pilihan);
+    if (pilihan != '1' || pilihan != '2' || pilihan != '3') {
+        printf("Masukkan data yang ingin dicari : ");
+        scanf("%d", &cari);
+    }
+    switch() {
+    case '1':
+        
+        break;
+    case '2':
+        break;
+    case '3':
+        break;
+    default:
+        printf("Pilihan tidak ada");
+        break;
+    }
+    return pilihan;
 }
 
 void dup(int x[], int y[]){
@@ -43,4 +61,19 @@ void generate(int arr[]) {
     puts("Melakukan generate...");
     for (int i = 0; i < n; i++)
         arr[i] = random() / 1000000;
+}
+
+void SeqSearch(int arr[], int cari){
+    int konfirm = 0;
+    int i = 0;
+    while (i < n && !konfirm) {
+        if (arr[i] == cari)
+            konfirm = 1;
+        else
+            i++;
+    }
+    if (konfirm)
+        printf("Data %d ditemukan pada urutan ke %d\n", cari, i+1);
+    else
+        printf("Data %d tidak ditemukan\n", cari);
 }
